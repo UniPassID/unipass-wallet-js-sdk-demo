750f81c3f89d53d4473a48c45bc213d713913655
李翔 <1058684043@qq.com>
2022-09-21 11:08:14 +0800

    fix: add json

--- src/hooks/useUnipass.ts
+++ src/hooks/useUnipass.ts
@@ -8,7 +8,7 @@ export function useUnipass() {
 
   useEffect(() => {
     try {
-      const provider = UnipassWalletProvider.getInstance({ env: "test" });
+      const provider = UnipassWalletProvider.getInstance({ env: "dev" });
       setUnipassWallet(provider);
     } catch (e: any) {
       message.error(e.message);
